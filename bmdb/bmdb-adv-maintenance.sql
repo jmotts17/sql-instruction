6dd65c5360905338a84f9e016ef656222a3742d0
Josh Motta <joshua.a.motta@gmail.com>
2020-08-23 22:02:24 -0400

    Seperated create and select statements

--- bmdb/bmdb-adv-maintenance.sql
+++ bmdb/bmdb-adv-maintenance.sql
@@ -1,7 +1,7 @@
 SELECT * FROM movie;
 SELECT * FROM actor;
 
-SELECT firstName, lastName, role, title, year, rating, director FROM credit
+SELECT firstName, lastName, role, title FROM credit
 INNER JOIN actor
 on credit.actorID = actor.ID
 INNER JOIN movie
@@ -38,19 +38,61 @@ INSERT INTO Actor (firstName, lastName, gender, birthdate) VALUES
 ('Heath', 'Ledger', 'M', '1979-04-04'),
 ('Ken', 'Watanbe', 'M', '1959-10-21'),
 ('Tom', 'Cruise', 'M', '1962-07-07'),
-('Christian', 'Bale', 'M', '1974-01-30');
+('Christian', 'Bale', 'M', '1974-01-30'),
+('Russell', 'Crowe', 'M', '1964-04-07'),
+('Joaquin', 'Phoenix', 'M', '1974-10-28'),
+('Clint', 'Eastwood', 'M', '1930-05-31'),
+('Bee', 'Vang', 'M', '1991-11-04'),
+('Elijah', 'Wood', 'M', '1981-01-28'),
+('Ian', 'McKellen', 'M', '1939-05-25'),
+('Martin', 'Freeman', 'M', '1971-09-08'),
+('Andy', 'Serkis', 'M', '1964-04-20'),
+('Tim', 'Robbins', 'M', '1958-10-16'),
+('Morgan', 'Freeman', 'M', '1937-06-01'),
+('Emily', 'Blunt', 'F', '1983-02-23'),
+('John', 'Krasinski', 'M', '1979-10-20'),
+('Scott', 'Eastwood', 'M', '1986-03-21'),
+('Orlando', 'Bloom', 'M', '1977-01-13');
 
 -- 3 add related credit entries
 INSERT INTO Credit (ActorID, MovieID, role) VALUES
-(5, 21, 'The Babe'),
-(6, 21, 'Scotty Smalls'),
-(7, 22, 'Ferris Bueller'),
-(8, 22, 'Cameron Frye'),
-(9, 23, 'Lisa'),
-(4, 23, 'Gary Wallace'),
-(10, 24, 'Clark'),
-(11, 24, 'Ellen'),
-(12, 25, 'Phil'),
-(13, 25, 'Rita');
-
--- Bonus: Classic Model - Full
+(5, 17, 'The Babe'),
+(6, 17, 'Scotty Smalls'),
+(7, 18, 'Ferris Bueller'),
+(8, 18, 'Cameron Frye'),
+(9, 19, 'Lisa'),
+(4, 19, 'Gary Wallace'),
+(10, 20, 'Clark'),
+(11, 20, 'Ellen'),
+(12, 21, 'Phil'),
+(13, 21, 'Rita'),
+(14, 3, 'Korben Dallas'),
+(15, 3, 'Leeloo'),
+(16, 4, 'Lane Meyer'),
+(17, 4, 'Al Meyer'),
+(18, 5, 'Bart'),
+(19, 5, 'Jim'),
+(20, 6, 'Woody'),
+(21, 6, 'Buzz Lightyear'),
+(22, 7, 'Benjamin Martin'),
+(23, 7, 'Gabriel Martin'),
+(24, 8, 'Katsumoto'),
+(25, 8, 'Nathan Algren'),
+(26, 9, 'Bruce Wayne'),
+(23, 9, 'Joker'),
+(27, 10, 'Maximus'),
+(28, 10, 'Commodus'),
+(29, 11, 'Walt Kowalski'),
+(30, 11, 'Thao'),
+(31, 12, 'Frodo'),
+(32, 12, 'Gandalf'),
+(40, 12, 'Legolas'),
+(33, 13, 'Bilbo'),
+(32, 13, 'Gandalf'),
+(34, 13, 'Gollum'),
+(35, 14, 'Andy Dufresne'),
+(36, 14, 'Ellis Boyd \'Red\' Redding'),
+(37, 15, 'Evelyn Abbott'),
+(38, 15, 'Lee Abbott'),
+(39, 16, 'SSG Clint Romesha'),
+(40, 16, 'CPT Ben Keating');
--- bmdb/bmdb-genre-create.sql
+++ bmdb/bmdb-genre-create.sql
@@ -1,6 +1,3 @@
-Drop table movieGenre;
-Drop table genre;
-
 Create table Genre (
 ID			integer			primary key auto_increment,
 name		varchar(30)		not null
@@ -14,8 +11,6 @@ FOREIGN KEY (movieID) references Movie(ID),
 FOREIGN KEY (genreID) references Genre(ID),
 CONSTRAINT gen_mov unique (movieID, genreID)
 );
-
-SELECT * FROM genre; 
 
 INSERT INTO Genre(name) VALUES 
 ('Action'),
@@ -35,29 +30,28 @@ INSERT INTO Genre(name) VALUES
 ('Superhero'),
 ('Western'),
 ('History'),
-('War');
+('War'),
+('Family');
 
 INSERT INTO MovieGenre (movieID, genreID) VALUES
-(1, 1),
-(1, 11),
-(1, 12),
-(2, 2),
-(2, 5),
-(3, 1),
-(3, 11),
-(3,3),
+(1, 1), (1, 11), (1, 12),
+(2, 2), (2, 5),
+(3, 1), (3, 11), (3,3),
 (4, 13),
-(5, 2),
-(5, 16),
-(6, 10),
-(6, 11),
-(6, 2),
-(7, 1),
-(7, 7),
-(7, 17),
-(8, 1),
-(8, 7),
-(8, 18),
-(9, 1),
-(9, 9),
-(9, 7);
+(5, 2), (5, 16),
+(6, 10), (6, 11), (6, 2),
+(7, 1), (7, 7), (7, 17), 
+(8, 1), (8, 7), (8, 18),
+(9, 1), (9, 9), (9, 7),
+(10, 1), (10, 11), (10, 7),
+(11, 7), 
+(12, 1), (12, 11), (12, 7),
+(13, 11), (13, 12),
+(14, 7),
+(15, 7), (15, 4), (15, 3),
+(16, 1), (16, 7), (16, 17),
+(17, 2), (17, 7), (17, 19),
+(18, 2),
+(19, 2), (19, 5), (19, 3),
+(20, 2),
+(21, 2), (21, 12), (21, 5);
