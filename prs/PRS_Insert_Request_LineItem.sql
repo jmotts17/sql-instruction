cb1b83cdcd1a7bd10273acdfbec23c54e5d6fcb3
Josh Motta <joshua.a.motta@gmail.com>
2020-08-26 21:54:51 -0400

    init commit

---
+++ prs/PRS_Insert_Request_LineItem.sql
@@ -0,0 +1,7 @@
+-- Insert data into Request Table
+INSERT INTO Request VALUES
+(1, 1, 'iPad Request', 'Mine was broken and returned.', '2020-09-01', 'Mail', 'Review', 299.99, '2020-08-26 08:00:00', null);
+
+-- Insert data into LineItem Table
+INSERT INTO LineItem VALUES
+(1, 1, 1, 1); 
