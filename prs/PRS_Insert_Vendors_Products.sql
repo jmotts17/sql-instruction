707f566d32bbe003325a7fbeff65a4303263de1f
Josh Motta <joshua.a.motta@gmail.com>
2020-09-08 22:17:31 -0400

    init commit

---
+++ prs/PRS_Insert_Vendors_Products.sql
@@ -0,0 +1,32 @@
+INSERT INTO Vendor VALUES
+(6, 'LE-1001', 'Lenovo', '1009 Think Place', 'Morrisville', 'NC', '27560', '855-253-6686', 'support@lenovo.com'),
+(7, 'DE-1001', 'Dell', 'One Dell Way', 'Round Rock', 'TX', '78682', '877-717-3355', 'us_sales@dell.com');
+
+INSERT INTO Product VALUES
+(14, 1, 'A2197', 'iPad 7th Generation', 429.99, null, null),
+(14, 2, 'A2197', 'iPad 7th Generation', 429.00, null, null),
+(14, 3, 'A2197', 'iPad 7th Generation', 399.98, null, null),
+(14, 5, 'A2197', 'iPad 7th Generation', 429.00, null, null),
+(15, 1, 'MUU32LL/A', 'iPad Mini 5th Generation', 499.99, null, null),
+(15, 2, 'MUU32LL/A', 'iPad Mini 5th Generation', 549.00, null, null),
+(15, 3, 'MUU32LL/A', 'iPad Mini 5th Generation', 499.99, null, null),
+(16, 1, 'MXK32LL/A', 'MacBook Pro - 13" Display with Touch Bar', 1299.99, null, null),
+(16, 2, 'MXK32LL/A', 'MacBook Pro - 13" Display with Touch Bar', 1299.00, null, null),
+(16, 3, 'MXK32LL/A', 'MacBook Pro - 13" Display with Touch Bar', 1299.99, null, null),
+(16, 5, 'MXK32LL/A', 'MacBook Pro - 13" Display with Touch Bar', 1229.99, null, null),
+(17, 1, 'MVH22LL/A', 'MacBook Air 13.3" Laptop with Touch ID', 1249.99, null, null),
+(17, 2, 'MVH22LL/A', 'MacBook Air 13.3" Laptop with Touch ID', 1299.00, null, null),
+(17, 5, 'MVH22LL/A', 'MacBook Air 13.3" Laptop with Touch ID', 1219.99, null, null),
+(18, 1, 'MRME2LL/A', 'Apple Magic Mouse 2', 99.99, null, null),
+(18, 2, 'MRME2LL/A', 'Apple Magic Mouse 2', 99.00, null, null),
+(18, 5, 'MRME2LL/A', 'Apple Magic Mouse 2', 99.99, null, null),
+(19, 1, 'MRMH2LL/A', 'Apple Magic Keyboard with Numeric Keypad', 149.99, null, null),
+(19, 2, 'MRMH2LL/A', 'Apple Magic Keyboard with Numeric Keypad', 149.00, null, null),
+(19, 3, 'MRMH2LL/A', 'Apple Magic Keyboard with Numeric Keypad', 149.00, null, null),
+(19, 5, 'MRMH2LL/A', 'Apple Magic Keyboard with Numeric Keypad', 149.99, null, null),
+(20, 1, '910-005647', 'Logitech MX Master 3 Wireless Laser Mouse', 99.99, null, null),
+(20, 3, '910-005647', 'Logitech MX Master 3 Wireless Laser Mouse', 99.99, null, null),
+(20, 5, '910-005647', 'Logitech MX Master 3 Wireless Laser Mouse', 99.99, null, null),
+(21, 1, '920-009295', 'Logitech MX Keys Advanced Wireless Illuminated Keyboard', 99.99, null, null),
+(21, 3, '920-009295', 'Logitech MX Keys Advanced Wireless Illuminated Keyboard', 99.99, null, null),
+(21, 5, '920-009295', 'Logitech MX Keys Advanced Wireless Illuminated Keyboard', 99.99, null, null);
